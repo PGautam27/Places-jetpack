b2d4b1e1defd0484613a22dfaa82c714ffd6f1f0
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-01 17:13:09 +0530

    New one

--- app/src/main/java/com/plcoding/mapscomposeguide/presentation/MapScreen.kt
+++ app/src/main/java/com/plcoding/mapscomposeguide/presentation/MapScreen.kt
@@ -12,8 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
 
 @Composable
 fun MapScreen(
@@ -41,8 +45,28 @@ fun MapScreen(
             properties = viewModel.state.properties,
             uiSettings = uiSettings,
             onMapLongClick = {
-
+                viewModel.onEvent(MapEvent.OnMapLongClick(it))
             }
-        )
+        ){
+            viewModel.state.parkingSpots.forEach { spot->
+                Marker(
+                    position = LatLng(spot.lat, spot.lng),
+                    title = "Parking spot (${spot.lat},${spot.lng})",
+                    snippet = "Long click to delete",
+                    onInfoWindowClick = {
+                        viewModel.onEvent(
+                            MapEvent.OnInfoWindowLongClick(spot)
+                        )
+                    },
+                    onClick = {
+                        it.showInfoWindow()
+                        true
+                    },
+                    icon = BitmapDescriptorFactory.defaultMarker(
+                        BitmapDescriptorFactory.HUE_BLUE
+                    )
+                )
+            }
+        }
     }
 }
