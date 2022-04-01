cbfd12dd2f389750c895127d5dd8963d900c9de5
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-01 15:13:46 +0530

    New one

--- app/src/main/java/com/plcoding/mapscomposeguide/presentation/MapState.kt
+++ app/src/main/java/com/plcoding/mapscomposeguide/presentation/MapState.kt
@@ -1,6 +1,5 @@
 package com.plcoding.mapscomposeguide.presentation
 
-import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.MapProperties
 
 data class MapState(
